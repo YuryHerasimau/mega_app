1ae9ccf2805cae3ef26dcd3b3764074d4b1dd091
Yury Herasimau <yl.herasimau@gmail.com>
2024-05-04 03:42:27 +0300

    Made wordcloud image to open in a modal window

--- static/js/imageOpen.js
+++ static/js/imageOpen.js
@@ -1,4 +1,4 @@
 document.getElementById('image-link').addEventListener('click', function(event) {
     event.preventDefault(); // Отмена действия по умолчанию
-    window.open(this.href, '_blank'); // Открывает ссылку в новом окне
+    $('#myModal').modal(); // Открываем модальное окно
 });
